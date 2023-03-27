e8233d232417539594e437f703abf2710a92cccb
Sceat <fetch@sceat.xyz>
2023-03-27 03:44:46 +0200

    feat: add items tooltip

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 zip -r ares-pack assets pack.mcmeta pack.png
-# mv ares-pack.zip ~/curseforge/minecraft/Instances/Sodium\ \(ares\)/resourcepacks/
+mv ares-pack.zip ~/curseforge/minecraft/Instances/Sodium\ \(ares\)/resourcepacks/
+# mv ares-pack.zip ~/curseforge/minecraft/Instances/vanilla/resourcepacks/
